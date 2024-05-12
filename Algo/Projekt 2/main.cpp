ce0e9ccb241afecf710fadb695e09d9b586edc39
Sapik <2002jakubs@gmail.com>
2024-05-13 00:01:20 +0200

    Bucketsort Done

--- Algo/Projekt 2/main.cpp
+++ Algo/Projekt 2/main.cpp
@@ -3,12 +3,14 @@
 #include "readFile.h"
 #include "movie.h"
 #include "quicksort.h"
+#include "bucketsort.h"
 #include <fstream>
 
 int main() {
     std::vector<Movie> movies;
     readFile("projekt2_dane.csv", movies, 10000);
-    quicksort(movies, 0, movies.size() - 1);
+    //quicksort(movies, 0, movies.size() - 1);
+    bucketsort(movies);
     std::cout << "Sorted movies by rating:" << std::endl;
     for (const auto &movie : movies) {
         std::cout << movie.title << " " << movie.rating << std::endl;
