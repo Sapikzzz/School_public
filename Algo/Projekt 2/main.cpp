2bffe6b12fe29011f4150875e8eee478ea94c94b
Sapik <2002jakubs@gmail.com>
2024-05-13 02:13:15 +0200

    Fixed issues

--- Algo/Projekt 2/main.cpp
+++ Algo/Projekt 2/main.cpp
@@ -10,22 +10,35 @@
 #include <chrono>
 
 int main() {
-    int amount = 1000000;
+    int amount = 10000;
     std::vector<Movie> movies_quick, movies_bucket, movies_merge;
     readFile("projekt2_dane.csv", movies_quick, amount);
     readFile("projekt2_dane.csv", movies_bucket, amount);
     readFile("projekt2_dane.csv", movies_merge, amount);
-    //quicksort(movies, 0, movies.size() - 1);
-    //bucketsort(movies);
-    //mergesort(movies, 0, movies.size() - 1);
 
     test_quick(movies_quick);
     test_bucket(movies_bucket);
     test_merge(movies_merge);
 
+    // quicksort(movies_quick, 0, movies_quick.size() - 1);
+    // bucketsort(movies_bucket);
+    // mergesort(movies_merge, 0, movies_merge.size() - 1);
+
+
+    // std::cout << "Quick movies by rating:" << std::endl;
+    // for (const auto &movie : movies_quick) {
+    //     std::cout << movie.title << " " << movie.rating << std::endl;
+    // }
+    // std::cout << std::endl;
     //
-    // std::cout << "Sorted movies by rating:" << std::endl;
-    // for (const auto &movie : movies) {
+    // std::cout << "Bucket movies by rating:" << std::endl;
+    // for (const auto &movie : movies_bucket) {
+    //     std::cout << movie.title << " " << movie.rating << std::endl;
+    // }
+    // std::cout << std::endl;
+    //
+    // std::cout << "Merge movies by rating:" << std::endl;
+    // for (const auto &movie : movies_merge) {
     //     std::cout << movie.title << " " << movie.rating << std::endl;
     // }
     return 0;
